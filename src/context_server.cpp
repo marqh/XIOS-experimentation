marqh/XIOS-experimentation

commit 5d49a99248c0c9ddae725f089ab032e47c3ab23f

Improvment of XIOS protocal on server side.
YM

git-svn-id: https://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1225 d613d254-d18a-4304-b296-5125d4c4d670

Former-commit-id: e47bf91d9d48b41055c1c409f8871103b728efbe

src/context_server.cpp
@@ -78,37 +78,59 @@ namespace xios
     MPI_Status status;
     map<int,CServerBuffer*>::iterator it;
 
-    for(rank=0;rank<commSize;rank++)
+    traceOff();
+    MPI_Iprobe(MPI_ANY_SOURCE, 20,interComm,&flag,&status);
+    traceOn();
+
+    if (flag==true)
     {
-      if (pendingRequest.find(rank)==pendingRequest.end())
+      rank=status.MPI_SOURCE ;
+
+      if (pendingRequest.find(rank)==pendingRequest.end()) listenPendingRequest(status) ;
+      else
       {
-        traceOff();
-        MPI_Iprobe(rank,20,interComm,&flag,&status);
-        traceOn();
-        if (flag==true)
+        for(rank=0;rank<commSize;rank++)
         {
-          it=buffers.find(rank);
-          if (it==buffers.end()) // Receive the buffer size and allocate the buffer
+          if (pendingRequest.find(rank)==pendingRequest.end())
           {
-            StdSize buffSize = 0;
-            MPI_Recv(&buffSize, 1, MPI_LONG, rank, 20, interComm, &status);
-            mapBufferSize_.insert(std::make_pair(rank, buffSize));
-            it=(buffers.insert(pair<int,CServerBuffer*>(rank,new CServerBuffer(buffSize)))).first;
-          }
-          else
-          {
-            MPI_Get_count(&status,MPI_CHAR,&count);
-            if (it->second->isBufferFree(count))
-            {
-              addr=(char*)it->second->getBuffer(count);
-              MPI_Irecv(addr,count,MPI_CHAR,rank,20,interComm,&pendingRequest[rank]);
-              bufferRequest[rank]=addr;
-            }
+
+            traceOff();
+            MPI_Iprobe(rank, 20,interComm,&flag,&status);
+            traceOn();
+            if (flag==true) listenPendingRequest(status) ;
           }
         }
       }
     }
   }
+
+  void CContextServer::listenPendingRequest(MPI_Status& status)
+  {
+    int count;
+    char * addr;
+    map<int,CServerBuffer*>::iterator it;
+    int rank=status.MPI_SOURCE ;
+
+    it=buffers.find(rank);
+    if (it==buffers.end()) // Receive the buffer size and allocate the buffer
+    {
+       StdSize buffSize = 0;
+       MPI_Recv(&buffSize, 1, MPI_LONG, rank, 20, interComm, &status);
+       mapBufferSize_.insert(std::make_pair(rank, buffSize));
+       it=(buffers.insert(pair<int,CServerBuffer*>(rank,new CServerBuffer(buffSize)))).first;
+    }
+    else
+    {
+      MPI_Get_count(&status,MPI_CHAR,&count);
+      if (it->second->isBufferFree(count))
+      {
+         addr=(char*)it->second->getBuffer(count);
+         MPI_Irecv(addr,count,MPI_CHAR,rank,20,interComm,&pendingRequest[rank]);
+         bufferRequest[rank]=addr;
+       }
+    }
+  }
+
 
   void CContextServer::checkPendingRequest(void)
   {
@@ -150,6 +172,7 @@ namespace xios
     size_t timeLine;
     map<size_t,CEventServer*>::iterator it;
 
+    CTimer::get("Process request").resume();
     while(count>0)
     {
       char* startBuffer=(char*)buffer.ptr();
@@ -163,6 +186,7 @@ namespace xios
       buffer.advance(size);
       count=buffer.remain();
     }
+    CTimer::get("Process request").suspend();
   }
 
   void CContextServer::processEvents(void)

src/context_server.hpp
@@ -16,6 +16,7 @@ namespace xios
     CContextServer(CContext* parent,MPI_Comm intraComm,MPI_Comm interComm) ;
     bool eventLoop(bool enableEventsProcessing = true);
     void listen(void) ;
+    void listenPendingRequest(MPI_Status& status) ;
     void checkPendingRequest(void) ;
     void processRequest(int rank, char* buff,int count) ;
     void processEvents(void) ;

commit 327faafc71f8f3539ebfbc04576cdfe5cb5fc215

Resolving a deadlock introduced in r1225.
git-svn-id: https://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1228 d613d254-d18a-4304-b296-5125d4c4d670

Former-commit-id: 19984e050970e6ebbeb6f5aa57871b54c62c37f1

src/context_server.cpp
@@ -85,9 +85,10 @@ namespace xios
     if (flag==true)
     {
       rank=status.MPI_SOURCE ;
-
-      if (pendingRequest.find(rank)==pendingRequest.end()) listenPendingRequest(status) ;
-      else
+      bool okLoop;
+      if (pendingRequest.find(rank)==pendingRequest.end())
+        okLoop = !listenPendingRequest(status) ;
+      if (okLoop)
       {
         for(rank=0;rank<commSize;rank++)
         {
@@ -104,7 +105,7 @@ namespace xios
     }
   }
 
-  void CContextServer::listenPendingRequest(MPI_Status& status)
+  bool CContextServer::listenPendingRequest(MPI_Status& status)
   {
     int count;
     char * addr;
@@ -118,6 +119,7 @@ namespace xios
        MPI_Recv(&buffSize, 1, MPI_LONG, rank, 20, interComm, &status);
        mapBufferSize_.insert(std::make_pair(rank, buffSize));
        it=(buffers.insert(pair<int,CServerBuffer*>(rank,new CServerBuffer(buffSize)))).first;
+       return true;
     }
     else
     {
@@ -127,7 +129,10 @@ namespace xios
          addr=(char*)it->second->getBuffer(count);
          MPI_Irecv(addr,count,MPI_CHAR,rank,20,interComm,&pendingRequest[rank]);
          bufferRequest[rank]=addr;
+         return true;
        }
+      else
+        return false;
     }
   }
 

src/context_server.hpp
@@ -16,7 +16,7 @@ namespace xios
     CContextServer(CContext* parent,MPI_Comm intraComm,MPI_Comm interComm) ;
     bool eventLoop(bool enableEventsProcessing = true);
     void listen(void) ;
-    void listenPendingRequest(MPI_Status& status) ;
+    bool listenPendingRequest(MPI_Status& status) ;
     void checkPendingRequest(void) ;
     void processRequest(int rank, char* buff,int count) ;
     void processEvents(void) ;