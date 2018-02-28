marqh/XIOS-experimentation

commit b0f57db7a31386fa049ccba6e080159a4739dfa6

Bugfix on reading of rectilinear domains:
taking into account that now scalar label is added to field coordinates.



git-svn-id: https://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1443 d613d254-d18a-4304-b296-5125d4c4d670

Former-commit-id: 7de01814d4620846ee7fa0cd0ef4913da30fd9f6

src/io/inetcdf4.cpp
@@ -495,7 +495,7 @@ namespace xios
   {
     std::list<StdString> coords = this->getCoordinatesIdList(name, path);
     std::list<StdString>::const_iterator it = coords.begin(), end = coords.end();
-    std::set<StdString> dimVarList;
+    int nb1Ddims = 0;
 
     for (; it != end; it++)
     {
@@ -503,18 +503,22 @@ namespace xios
       if (this->hasVariable(coord, path) && !this->isTemporal(coord, path))
       {
         std::map<StdString, StdSize> dimvar = this->getDimensions(&coord, path);
-//        if ((dimvar.size() == 1) && (dimvar.find(coord) != dimvar.end()))
         if ((dimvar.size() == 1))
         {
-          dimVarList.insert(dimvar.begin()->first);
-          continue;
+          // Check that the coordinate is not a scalar label
+          StdString dimTmp = dimvar.begin()->first;
+          if (dimTmp.compare("str_len") != 0)
+          {
+            nb1Ddims++;
+            continue;
+          }
         }
         else
           return false;
       }
     }
 
-    return (dimVarList.size() != 1);
+    return (nb1Ddims != 1);
   }
 
   bool CINetCDF4::isCurvilinear(const StdString& name, const CVarPath* const path)

commit 0dd803608f2051e8fe5df1906cfed4727de680aa

Correcting a bug on reading unstructured domains introduced in r1443.
(Whatever was working should continue to work.)


git-svn-id: https://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/dev/XIOS_DEV_CMIP6@1444 d613d254-d18a-4304-b296-5125d4c4d670

Former-commit-id: 22a458179ae308d4b3fcca452605517c42a4ba1d

src/io/inetcdf4.cpp
@@ -495,7 +495,7 @@ namespace xios
   {
     std::list<StdString> coords = this->getCoordinatesIdList(name, path);
     std::list<StdString>::const_iterator it = coords.begin(), end = coords.end();
-    int nb1Ddims = 0;
+    std::set<StdString> dimVarList;
 
     for (; it != end; it++)
     {
@@ -509,7 +509,7 @@ namespace xios
           StdString dimTmp = dimvar.begin()->first;
           if (dimTmp.compare("str_len") != 0)
           {
-            nb1Ddims++;
+            dimVarList.insert(dimvar.begin()->first);
             continue;
           }
         }
@@ -518,7 +518,7 @@ namespace xios
       }
     }
 
-    return (nb1Ddims != 1);
+    return (dimVarList.size() != 1);
   }
 
   bool CINetCDF4::isCurvilinear(const StdString& name, const CVarPath* const path)